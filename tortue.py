2078a789bd7348d1ecf81f7e942c53d4592f776b
Pauline BEHAR <p.behar@saint-louis29.net>
2025-11-18 16:04:37 +0100

    fonction ajout fonctionne

--- tortue.py
+++ tortue.py
@@ -21,17 +21,23 @@ def carre(longueur):
     polygone(longueur, 4)
     
 
-def polygone(longueur, nb_cotes):
+def polygone(longueur, nb_cotes, ajout=0):
     """
     longueur(int) : la longueur du côté du polygone
     nb_cotes : le nombre de côtés du polygone
+    ajout : 
     dessine un polygone de longueur de côtés <longueur> et de nombre de côtés <nb_cotes>
     """
     for _ in range(nb_cotes):
+        longueur += ajout
         t.forward(longueur)
         t.right(360/nb_cotes)
-#polygone(100, 6)
-equilateral(100)
+        
+polygone(100, 6, 10)
+
+
+
+
 
 turtle.exitonclick()
 
