a5a7102b5cfeaa11ad5da7c95c56eed68b9a59be
Pauline BEHAR <p.behar@saint-louis29.net>
2025-11-18 16:14:44 +0100

    ajout déviation

--- tortue.py
+++ tortue.py
@@ -21,19 +21,21 @@ def carre(longueur):
     polygone(longueur, 4)
     
 
-def polygone(longueur, nb_cotes, ajout=0):
+def polygone(longueur, nb_cotes, ajout=0, deviation=0):
     """
     longueur(int) : la longueur du côté du polygone
     nb_cotes : le nombre de côtés du polygone
     ajout : 
     dessine un polygone de longueur de côtés <longueur> et de nombre de côtés <nb_cotes>
     """
+    total_dev = 0
     for _ in range(nb_cotes):
         longueur += ajout
         t.forward(longueur)
-        t.right(360/nb_cotes)
+        t.right(360/nb_cotes + total_dev)
+        total_dev = total_dev + deviation
         
-polygone(100, 6, 10)
+polygone(100, 4,deviation = -10)
 
 
 
